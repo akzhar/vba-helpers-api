5fd145ff0158ea8f6c3fce3fe5d6f68940697a12
Akzhar Taov <akzhar.taov@yandex.ru>
2022-06-29 01:53:12 +0300

    Update routes according with changes in data

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 - [GET /helpers](https://vba-helpers-api.herokuapp.com/api/helpers) - получение хелперов
 - [GET /helpers/search-by/category/:keyword](https://vba-helpers-api.herokuapp.com/api/helpers/search-by/category/числа) - поиск хелперов по категории
 - [GET /helpers/search-by/title/:keyword](https://vba-helpers-api.herokuapp.com/api/helpers/search-by/title/получить%20индекс) - поиск хелперов по заголовку
-- [GET /helpers/search-by/tag/:keyword](https://vba-helpers-api.herokuapp.com/api/helpers/search-by/tag/массив) - поиск хелперов по тэгу
-- [GET /tags/search-by/category/:keyword](https://vba-helpers-api.herokuapp.com/api/tags/search-by/category/строки) - поиск тэгов по категории
-- [GET /categories](https://vba-helpers-api.herokuapp.com/api/categories) - получение категорий хелперов
-- [GET /categories/search/:keyword](https://vba-helpers-api.herokuapp.com/api/categories/search/строки) - поиск категорий по имени
+- [GET /helpers/search-by/keyword/:keyword](https://vba-helpers-api.herokuapp.com/api/helpers/search-by/keyword/sort%20array) - поиск хелперов по ключевым словам (фразе)
+- [GET /keywords/search-by/category/:keyword](https://vba-helpers-api.herokuapp.com/api/keywords/search-by/category/конвертация) - получение списка ключевых слов (фраз) по категории
+- [GET /categories](https://vba-helpers-api.herokuapp.com/api/categories) - получение списка категорий
+- [GET /categories/search/:keyword](https://vba-helpers-api.herokuapp.com/api/categories/search/строки) - поиск категории по имени
 
 ## 1. Установка зависимостей
 Находясь в корневой папке проекта, выполните команду:
--- src/getRoutedData.js
+++ src/getRoutedData.js
@@ -3,28 +3,35 @@
 // ф-ция разбивает данные (json) по роутам
 function getRoutedData(json) {
   
-  let [ categories, tagsByCategory ] = json.reduce(([ categories, tagsByCategory ], item) => {
+  let [ categories, keywordsByCategory ] = json.reduce(([ categories, keywordsByCategory ], item) => {
     
-    const { category: itemCategories, tags: itemTags } = item;
+    const { category: itemCategories, _keywords: itemKeywords } = item;
 
-    itemCategories.forEach(item => {
-      if (!Object.hasOwnProperty.call(tagsByCategory, item)) {
-        tagsByCategory[item] = itemTags;
+    const keywords = itemKeywords.split('\n');
+
+    itemCategories.forEach(category => {
+
+      const categoryExists = Boolean(Object.hasOwnProperty.call(keywordsByCategory, category));
+
+      if (categoryExists) {
+        keywordsByCategory[category] = keywordsByCategory[category].concat(keywords);
       } else {
-        tagsByCategory[item].concat(itemTags);
+        keywordsByCategory[category] = keywords;
       }
-      tagsByCategory[item] = [...new Set(tagsByCategory[item])];
+
+      keywordsByCategory[category] = [...new Set(keywordsByCategory[category])];
+
     });
 
-    return [ categories, tagsByCategory ];
+    return [ categories, keywordsByCategory ];
 
   }, [ {}, {} ]);
 
-  categories = Object.keys(tagsByCategory);
+  categories = Object.keys(keywordsByCategory);
   categories = categories.map((item, i) => ({ id: i, category: item }));
-  tagsByCategory = Object.keys(tagsByCategory).map((key, i) => ( { id: i, category: key, tags: tagsByCategory[key] }));
+  keywordsByCategory = Object.keys(keywordsByCategory).map((key, i) => ( { id: i, category: key, keywords: keywordsByCategory[key] }));
 
-  return { helpers: json, categories, tagsByCategory };
+  return { helpers: json, categories, keywordsByCategory };
 }
 
 module.exports = getRoutedData;
